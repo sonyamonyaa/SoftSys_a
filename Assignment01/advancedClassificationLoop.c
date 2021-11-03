ea97f2d0531071701adab4b2e238f4559e7492d7
Sonya <74297783+sonyamonyaa@users.noreply.github.com>
2021-11-03 14:19:50 +0200

    isArmstrong implementation

--- Assignment01/advancedClassificationLoop.c
+++ Assignment01/advancedClassificationLoop.c
@@ -1,10 +1,19 @@
 #include <NumClass.h>
 #include <math.h>
-/*self note: loop implmentation*/
+//self note: loop implmentation
 int isArmstrong(int num) {
-    
+    int n = (num==0)?1:log10(num)+1; 
+    int temp = num;
+    int digit,sum = 0;
+    while (temp > 0)
+    {
+        digit = temp % 10;
+        sum += pow(digit,n);
+        temp /= 10;
+    }
+    return sum == num;
 }
 
 int isPalindrome(int num){
-
+    
 }
--- Assignment01/advancedClassificationRecursion.c
+++ Assignment01/advancedClassificationRecursion.c
@@ -1,9 +1,33 @@
 #include <NumClass.h>
 #include <math.h>
-int isArmstrong(int num) {
+//self note: recursion implementation
 
+/*returns the sum of digits of a num to the power of n,
+using recursion.
+*/
+int recPowSum(int num, int n)
+{
+    int sum;
+    if (num == 0)
+        return 0;
+    if (num < 10)
+        return pow(num, n);
+    else
+    {
+        int digit = num % 10;
+        sum += pow(digit, n);
+        sum +=recPowSum(num/10,n);
+    }
+    return sum;
+}
+int isArmstrong(int num)
+{
+    int n = (num == 0) ? 1 : log10(num) + 1;
+    int digit;
+    int sum = recPowSum(num,n);
+    return sum == num;
 }
 
-int isPalindrome(int num){
-    
+int isPalindrome(int num)
+{
 }
