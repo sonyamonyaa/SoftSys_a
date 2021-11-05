88ee3ee1445cdc0d90d39a4dd85577c2842e198a
Sonya <74297783+sonyamonyaa@users.noreply.github.com>
2021-11-05 12:16:10 +0200

    main implementation

--- Assignment01/main.c
+++ Assignment01/main.c
@@ -1,5 +1,38 @@
 #include <NumClass.h>
+#include <stdio.h>
 int main(){
-    int first, last;
-    
+    int fst, lst;
+    scanf("%d,%d",&fst,&lst);
+    printf("The Armstrong numbers are:");
+    for (int i = fst; i < lst; i++)
+    {
+        if (isArmstrong(i))
+        {
+            printf(" %d",i);
+        }
+    }
+    printf("/nThe Palindromes are:");
+    for (int i = fst; i < lst; i++)
+    {
+        if (isPalindrome(i))
+        {
+            printf(" %d",i);
+        }
+    }
+    printf("/nThe Prime numbers are:");
+    for (int i = fst; i < lst; i++)
+    {
+        if (isPrime(i))
+        {
+            printf(" %d",i);
+        }
+    }
+    printf("/nThe Strong numbers are:");
+    for (int i = fst; i < lst; i++)
+    {
+        if (isStrong(i))
+        {
+            printf(" %d",i);
+        }
+    }
 }
